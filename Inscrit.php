b2b9b734eca0d01bde4e6c75c1a0e7f59d3ee9ca
visayze <alexandre.visayze.tic@gmail.com>
2017-06-20 17:31:03 +0200

    Update Json/Inscrit

--- Inscrit.php
+++ Inscrit.php
@@ -1,11 +1,12 @@
 <?php
-session_start();
+
 
 require 'bdd.inc.php';
+require 'Json.php';
 
-
+/*
 $email = $_POST['email'];
-$mdp = $_POST['mdp'];
+$mdp = $_POST['mdp'];*/
 $_SESSION['login']= "";
 
 
@@ -14,69 +15,74 @@ class Inscrit
 	private $_email;
 	private $_mdp;
 
-	public function setEmail($email)
+	//permet de modifier la valeur de $_email de la classe Inscrit
+    public function setEmail($email)
   {
-    /*if (gettype($email) == 'string') // S'il ne s'agit pas d'un nombre entier.
+    
+    if (gettype($email) == 'string') // S'il ne s'agit pas d'un string.
     {
       trigger_error('Le mail n\'est pas du type String' , E_USER_WARNING);
       return;
-    }*/
+    }
         
     $this->_email = $email;
   }
+    //permet de modifier la valeur de $_mdp de la classe Inscrit
   	public function setMdp($mdp)
   {
-    /*if (gettype($email) == 'string') // S'il ne s'agit pas d'un nombre entier.
+    if (gettype($mdp) == 'string') // Test si c'est une chaine de caractère(sting).
     {
-      trigger_error('Le mail n\'est pas du type String' , E_USER_WARNING);
+      trigger_error('Le mot de passe n\'est pas du type String' , E_USER_WARNING);
       return;
-    }*/
+    }
         
     $this->_mdp = $mdp;
   }
   	public function setlogin($bdd){
+        $tmp = TraitementJson::RecupJson(/*"data.json"*/);
+        print_r($tmp);
       $id="";
-    $requete=$bdd->prepare('SELECT * FROM inscrit WHERE mail_inscrit=? AND mdp_inscrit=?') or die (print_r($bdd->errorInfo()));
-    $requete->execute(array($_POST['email'],$_POST['mdp']));
+    $requete=$bdd->prepare('SELECT id FROM inscrit WHERE mail_inscrit= :mail_inscrit AND mdp_inscrit= :mdp_inscrit') or die (print_r($bdd->errorInfo()));
+        if($tmp){
+    $requete->execute($tmp);
+        }
     while ($data=$requete->fetch())
     {
       $id=$data['id'];
+        echo TraitementJson::EnvoiJson($requete);
     }
     if ($id=="")
     {
-      echo 'Erreur ! Votre identifiant ou votre mot de passe est mal saisi.';
+      echo 'Erreur ! Votre email ou votre mot de passe est mal saisi.';
     }else{
-      $_SESSION['login'] = $_POST['email'];
-      echo "Vous êtes connecté avec le mail suivant:",$_SESSION['login'];
+      $_SESSION['id'] = $id;
+      echo "Vous êtes connecté avec l'id suivant:",$_SESSION['id'];
     }
+        
     $requete->closeCursor();
   }
+    //permet de retourner email
     public function email()
   {
   	return $this->_email;
   }
+    //permet de retourner mdp
      public function mdp()
   {
   	return $this->_mdp;
   }
+    //permet de retourner la valeur de login
   public function login()
   {
     return $_SESSION['login'];
   }
-
-  //fonction pour afficher la liste d'emploi
-  //problem ne fait pas la difference entre les deux types d'offre
-  //attention a l'id
-  
-
 }
 
 $Inscrit = new Inscrit;
-$Inscrit->setEmail($email);
-$Inscrit->setMdp($mdp);
+/*$Inscrit->setEmail($email);
+$Inscrit->setMdp($mdp);*/
 $Inscrit->setlogin($bdd);
-$Inscrit->login();
-$Annonce = new 
+//$Inscrit->login();
 
 
 
--- Json.php
+++ Json.php
@@ -7,9 +7,9 @@ class TraitementJson
   private $_json;
   
   //Fonction qui permet la récupération d'un doc JSON
-  public function RecupJson($url){
+  public static function RecupJson(/*$url*/){
 
-    $json = file_get_contents($url,0,null,null); //"php://input"
+    $json = file_get_contents("php://input"); //$url,0,null,null
     $tmp = json_decode($json, true);
     return $tmp;//récuperation automaique
    //$requete=$bdd->prepare('INSERT INTO inscrit /*(id, mail_inscrit, mdp_inscrit )*/ VALUES (:id,:mail_inscrit,:mdp_inscrit )')or die (print_r($bdd->errorInfo()));//null auto génère
@@ -22,26 +22,24 @@ class TraitementJson
     }//$a = array ('a' => 'apple', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
 //print_r ($a);
 
-  public function EnvoiJson($requete){
-
-  //  $requete=$bdd->prepare('SELECT * FROM inscrit/*users WHERE mail_inscrit=? AND mdp_inscrit=?*/') or die (print_r($bdd->errorInfo()));
-  //  $requete->execute(/*array($_POST['email'],$_POST['mdp'])*/);
-    
+  public static function EnvoiJson($requete){
     // fetch the results into an array
     $result = $requete->fetchAll( PDO/*bdd*/::FETCH_ASSOC );
     // convert to json
+      //print_r($result);
     $json = json_encode( $result );
 
     // echo the json string
-    echo $json;
+      return $json;
+      
 
     $requete->closeCursor();
 
   }
 }
-
-$JsonFile = new Json;
+/*
+$JsonFile = new TraitementJson;
 $JsonFile->EnvoiJson($bdd);
 $JsonFile->RecupJson("data.json");
-
+*/
 ?>
